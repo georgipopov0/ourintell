b3558eca2d6ec02dbbc8392277dfcc0be91349e0
unknown <georgipopov069@gmail.com>
2021-02-08 08:01:42 +0200

    [refactor] Simplified the database layout

--- ourintell/schema.sql
+++ ourintell/schema.sql
@@ -1,4 +1,3 @@
-drop database threat_data;
 create database threat_data;
 use threat_data;
 
@@ -8,60 +7,30 @@ create table RecordedEvents(
 );
 
 create table TicketingMethods(
-	methodId int not null primary key unique auto_increment,
-	ticketingMethod varchar(256) not  null,
+	ticketingMethod varchar(256) not null primary key unique,
     methodDescription varchar(512)  
 );
 
-create table roles(
-	roleID int not null primary key unique auto_increment,
-    roleName varchar(64),
-    canView bool,
-    canEdit bool
-);
-
 create table TrackableResources(
-	resurceName varchar(128) not null primary key unique,
-    dataType varchar(64) not null,
-    onEventTriger bool not null
-);
-
-create table Organisations(
-	organisationId int primary key not null auto_increment,
-	organisationName varchar(512) not null unique,
-    credentials varchar(1024) not null,
-    email varchar(254) not null
+	resurceName varchar(128) not null primary key unique
 );
 
 create table Users(
 	userId int primary key not null auto_increment,
 	userName varchar(512) not null unique,
-    credentials varchar(1024) not null,
-    email varchar(254),
-    organisationId int,
-    foreign key(organisationId) references Organisations(organisationId)
+    password varchar(1024) not null,
+    email varchar(254)
 );
 
+drop table Subscriptions;
 create table Subscriptions(
 	subscriptionId int not null auto_increment unique primary key,
     trackedResource varchar(128),
-    ticketingMethodId int,
+    ticketingMethod varchar(256),
     ticketignAdress varchar(256),
-    ticketFrequency int not null,
-    enrollmentDate date not null,
-    expiryDate date,
-    price float4 not null,
     
     foreign key(trackedResource) references TrackableResources(resurceName),
-    foreign key(ticketingMethodId) references TicketingMethods(methodId)
-);
-
-create table UserRoles(
-	roleId int,
-    userId int,
-    
-	foreign key(roleId) references Roles(roleId),
-    foreign key(userId) references Users(userId)
+    foreign key(ticketingMethod) references TicketingMethods(ticketingMethod)
 );
 
 create table UserSubscriptions(
@@ -72,6 +41,12 @@ create table UserSubscriptions(
 	foreign key(subscriptionId) references Subscriptions(subscriptionId)
 );
 
-
+create table reportedEvents(
+    eventId varchar(256),  
+	subscriptionId int,
+    
+	foreign key(eventId) references RecordedEvents(eventId),
+    foreign key(subscriptionId) references Subscriptions(subscriptionId)
+)
 
 
